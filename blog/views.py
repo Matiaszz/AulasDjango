5a6607f84eb4bd7315fdf25c7ef1f784a8f6a9df
Allan <alangiovanepaes@gmail.com>
2024-08-24 18:08:44 -0300

    templates/html em django

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,9 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
-def blog(request) -> HttpResponse:
-    return HttpResponse('BLOG APP')
+def blog(request):
+    return render(request, 'blog/index.html')
 
 
-def articles(request) -> HttpResponse:
-    return HttpResponse('ARTICLES')
+def articles(request):
+    return render(request, 'blog/articles.html')
--- home/views.py
+++ home/views.py
@@ -1,6 +1,5 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
-def home(request) -> HttpResponse:
-    return HttpResponse('HOME APP')
+def home(request):
+    return render(request, 'home/index.html')
